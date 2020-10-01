a6d558863656def92311a7eae2c2990cd23a3fac
training.ekoura <godwin.avodagbe@ekoura.com>
2020-10-01 11:31:00 +0200

    new build

---
+++ storybook-static/precache-manifest.cf72a8c6112bdbd8dbecccc66272cdb4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8aea863ec12005a05590707be01567b6",
+    "url": "iframe.html"
+  },
+  {
+    "url": "main.8b85d6a6e4b41ebae6fa.bundle.js"
+  },
+  {
+    "url": "runtime~main.8b85d6a6e4b41ebae6fa.bundle.js"
+  },
+  {
+    "revision": "f0d4c2b22040e9397d52",
+    "url": "static/css/main.67471bcc.chunk.css"
+  },
+  {
+    "url": "vendors~main.8b85d6a6e4b41ebae6fa.bundle.js"
+  },
+  {
+    "url": "vendors~main.8b85d6a6e4b41ebae6fa.bundle.js.LICENSE.txt"
+  }
+]);
--- storybook-static/service-worker.js
+++ storybook-static/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "precache-manifest.7a6d40df60fe24ee3ef2a4621d68d16d.js"
+  "precache-manifest.cf72a8c6112bdbd8dbecccc66272cdb4.js"
 );
 
 self.addEventListener('message', (event) => {
